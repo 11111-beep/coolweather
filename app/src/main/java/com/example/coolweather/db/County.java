311fbdf8b4355c2d37c8de74d03932f3bcdd2fad
HQW <1920634907@qq.com>
2025-03-03 23:05:12 +0800

    加入创建数据库和表的各项配置.

---
+++ app/src/main/java/com/example/coolweather/db/County.java
@@ -0,0 +1,33 @@
+package com.example.coolweather.db;
+
+public class County {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getCountyName() {
+        return countyName;
+    }
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+    public String getweatherId(){
+        return weatherId;
+    }
+
+    public void setweatherId(String weatherId){
+        this.weatherId = weatherId;
+    }
+    public int getCityId(){
+        return cityId;
+    }
+    public void setCityId(int cityId){
+        this.cityId = cityId;
+    }
+}
